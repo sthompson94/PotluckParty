c3ce435e6de4c525ff9ae75bd9a5e752387f5a0d
Sam Thompson <electronic62794@yahoo.com>
2020-01-29 10:32:30 -0600

    heroku

--- sql/connection.js
+++ sql/connection.js
@@ -3,14 +3,10 @@ var mysql = require("mysql");
 // pass through the cridentials to connect to mysql and give the intended database
 var connection = mysql.createConnection({
     
-    host: "localhost",
-
-    port: 3306,
-
-    user: "root",
-
-    password: "password",
-    database: "potluck_db"
+    HOST: "us-cdbr-iron-east-04.cleardb.net",
+    USER: "b990e4bc318ffb",
+    PASSWORD: "388fcb5e",
+    DB: "heroku_80a7bb762a27144"
 
 });
 
