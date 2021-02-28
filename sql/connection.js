8c52198858ea90ebbaa1949e0ac16356eac4f327
Sam Thompson <samthompson62794@gmail.com>
2021-02-28 13:39:52 -0600

    works locally

--- sql/connection.js
+++ sql/connection.js
@@ -33,6 +33,15 @@ connection.connect(function(err){
     console.log('error when connecting to db:', err);
     setTimeout(handleDisconnect, 2000);
 
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+      });
+
     console.log("Connection Successful");
     afterConnect();
 })
