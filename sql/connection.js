bc1ea46e40099ddddd19fde83a390a599b04ab09
Sam Thompson <samthompson62794@gmail.com>
2021-02-28 13:32:41 -0600

    hopefully fixed disconnect issue

--- sql/connection.js
+++ sql/connection.js
@@ -1,36 +1,6 @@
 var mysql = require("mysql");
 
 // pass through the cridentials to connect to mysql and give the intended database
-// var connection = mysql.createConnection({
-    
-//     host: "us-cdbr-iron-east-04.cleardb.net",
-
-//     port: 3306,
-
-//     user: "bb148d41a42cd6",
-
-//     password: "501a8c14",
-//     database: "heroku_fff6ebb5fb33f80"
-
-// });
-
-// // mysql://bb148d41a42cd6:501a8c14@us-cdbr-iron-east-04.cleardb.net/heroku_fff6ebb5fb33f80?reconnect=true
-
-// connection.connect(function(err){
-//     if(err) throw err;
-
-//     console.log("Connection Successful");
-//     afterConnect();
-// })
-
-// function afterConnect(){
-//     connection.query("SELECT * FROM people", function(err, res){
-//         if(err) throw err;
-
-//         console.log(res);
-
-//     });
-// }
 
 var db_config = {
     host: "us-cdbr-iron-east-04.cleardb.net",
@@ -42,6 +12,41 @@ var db_config = {
     password: "501a8c14",
     database: "heroku_fff6ebb5fb33f80"
   };
+  
+var connection = mysql.createConnection({
+    
+    host: "us-cdbr-iron-east-04.cleardb.net",
+
+    port: 3306,
+
+    user: "bb148d41a42cd6",
+
+    password: "501a8c14",
+    database: "heroku_fff6ebb5fb33f80"
+
+});
+
+// mysql://bb148d41a42cd6:501a8c14@us-cdbr-iron-east-04.cleardb.net/heroku_fff6ebb5fb33f80?reconnect=true
+
+connection.connect(function(err){
+    if(err)
+    console.log('error when connecting to db:', err);
+    setTimeout(handleDisconnect, 2000);
+
+    console.log("Connection Successful");
+    afterConnect();
+})
+
+function afterConnect(){
+    connection.query("SELECT * FROM people", function(err, res){
+        if(err) throw err;
+
+        console.log(res);
+
+    });
+}
+
+
 
 function handleDisconnect() {
     connection = mysql.createConnection(db_config); // Recreate the connection, since
