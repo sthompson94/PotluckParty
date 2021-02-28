b756ce4791a31953491d8f695c1747a7967b0853
Sam Thompson <samthompson62794@gmail.com>
2021-02-28 13:17:26 -0600

    trying to fix heroku server error

--- sql/connection.js
+++ sql/connection.js
@@ -1,36 +1,36 @@
 var mysql = require("mysql");
 
 // pass through the cridentials to connect to mysql and give the intended database
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
     
-    host: "us-cdbr-iron-east-04.cleardb.net",
+//     host: "us-cdbr-iron-east-04.cleardb.net",
 
-    port: 3306,
+//     port: 3306,
 
-    user: "bb148d41a42cd6",
+//     user: "bb148d41a42cd6",
 
-    password: "501a8c14",
-    database: "heroku_fff6ebb5fb33f80"
+//     password: "501a8c14",
+//     database: "heroku_fff6ebb5fb33f80"
 
-});
+// });
 
-// mysql://bb148d41a42cd6:501a8c14@us-cdbr-iron-east-04.cleardb.net/heroku_fff6ebb5fb33f80?reconnect=true
+// // mysql://bb148d41a42cd6:501a8c14@us-cdbr-iron-east-04.cleardb.net/heroku_fff6ebb5fb33f80?reconnect=true
 
-connection.connect(function(err){
-    if(err) throw err;
+// connection.connect(function(err){
+//     if(err) throw err;
 
-    console.log("Connection Successful");
-    afterConnect();
-})
+//     console.log("Connection Successful");
+//     afterConnect();
+// })
 
-function afterConnect(){
-    connection.query("SELECT * FROM people", function(err, res){
-        if(err) throw err;
+// function afterConnect(){
+//     connection.query("SELECT * FROM people", function(err, res){
+//         if(err) throw err;
 
-        console.log(res);
+//         console.log(res);
 
-    });
-}
+//     });
+// }
 
 var db_config = {
     host: "us-cdbr-iron-east-04.cleardb.net",
